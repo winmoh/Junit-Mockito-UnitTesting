f02f905b387cc074c9d33fc7317d69a62b29674d
winmoh <mohamedaouinti789@gmail.com>
2023-11-14 01:36:14 +0100

    Student repo,getting all,adding service,existing email exception raise welll configured

--- src/test/java/com/example/demo/test/studentServRepoV1.java
+++ src/test/java/com/example/demo/test/studentServRepoV1.java
@@ -5,6 +5,7 @@ import com.example.demo.student.Gender;
 import com.example.demo.student.Student;
 import com.example.demo.student.StudentRepository;
 import com.example.demo.student.StudentService;
+import com.example.demo.student.exception.BadRequestException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,6 +18,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 
@@ -60,5 +63,20 @@ public class studentServRepoV1 {
         verify(SRepo).save(argCaptor.capture());
         Student capStd=argCaptor.getValue();
         assertThat(std).isEqualTo(capStd);
+    }
+
+    @Test
+    public void EmailExistingexeptionRaisinTest(){
+        //given
+        Student std=new Student("ayoubiz","ayoubiz@gmail.com", Gender.MALE);
+
+        given(SRepo.selectExistsEmail(std.getEmail()))
+                .willReturn(true);
+        //when
+        //then
+        assertThatThrownBy(()->SService.addStudent(std))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessageContaining("Email " + std.getEmail() + " taken");
+
     }
 }
