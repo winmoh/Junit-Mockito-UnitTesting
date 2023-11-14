a08198300324162e7b31bbce0cb975333c0a684f
winmoh <mohamedaouinti789@gmail.com>
2023-11-14 13:35:02 +0100

    deleteporcess is tested

--- src/test/java/com/example/demo/test/studentServRepoV1.java
+++ src/test/java/com/example/demo/test/studentServRepoV1.java
@@ -6,6 +6,7 @@ import com.example.demo.student.Student;
 import com.example.demo.student.StudentRepository;
 import com.example.demo.student.StudentService;
 import com.example.demo.student.exception.BadRequestException;
+import com.example.demo.student.exception.StudentNotFoundException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,8 +20,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -70,7 +72,7 @@ public class studentServRepoV1 {
         //given
         Student std=new Student("ayoubiz","ayoubiz@gmail.com", Gender.MALE);
 
-        given(SRepo.selectExistsEmail(std.getEmail()))
+        given(SRepo.selectExistsEmail(anyString()))
                 .willReturn(true);
         //when
         //then
@@ -78,5 +80,38 @@ public class studentServRepoV1 {
                 .isInstanceOf(BadRequestException.class)
                 .hasMessageContaining("Email " + std.getEmail() + " taken");
 
+        verify(SRepo,never()).save(std);
+
     }
+
+    @Test
+    public void testDeeleteService(){
+        //given
+        long id =1L;
+        //when
+        given(SRepo.existsById(id)).willReturn(true);
+        doNothing().when(SRepo).deleteById(id);
+
+        SService.deleteStudent(id);
+
+        //then
+        verify(SRepo).deleteById(id);
+
+    }
+
+    @Test
+    public void testStudentNotExistingExceptionRaise(){
+        //when
+        long id=1L;
+        //then
+        given(SRepo.existsById(anyLong())).willReturn(false);
+        //then
+        assertThatThrownBy(()->SService.deleteStudent(id))
+                .isInstanceOf(StudentNotFoundException.class)
+                .hasMessageContaining("Student with id " + id + " does not exists");
+
+        verify(SRepo,never()).deleteById(id);
+    }
+
+
 }
